495c08cf8b924d8085f903f1223686e4f5e19b4a
molla <molla@gmail.com>
2020-01-15 22:29:27 +0200

    creat data base

--- server/server.js
+++ server/server.js
@@ -3,14 +3,31 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
-app.get('/', (req,res) => {
-    res.send('<h1>hello world</h1>');
-});
 
-app.post('/account/users', (req, res) => {
-    console.log(req.body);
+
+// Data base
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/cinema";
+
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("cinema");
+    app.get('/', (req, res) => {
+        res.send('"hii');
+    });
+
+    app.post('/account/users', (req, res) => {
+        console.log(req.body);
+        dbo.collection("users").insertOne(req.body, (err, user)=>{
+            if (err) {
+                console.log(err);
+                res.send("there is some issue");
+            }
+            res.send(user);
+        })
+    });
 });
 
 // Port
