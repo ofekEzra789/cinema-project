af5c47a73a61cb19aaedee838f15cee253156c43
molla <molla@gmail.com>
2020-01-22 10:02:02 +0200

    fix the server

---
+++ server/routeHelper.js
@@ -0,0 +1,78 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/";
+const dbName = 'cinema', collectionName = 'users';
+
+
+function login(req, res) {
+    console.log("/users/login is accessed");
+
+    MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        const dbo = db.db(dbName);
+
+        // --- expecting email , passeord
+        const queryUser = req.body;
+
+        dbo.collection(collectionName).findOne({email:queryUser.email,password:queryUser.password}, function (err, user) {
+            if (err) {
+                console.log(err);
+                return res.sendStatus(500);
+            }
+
+            if (user) {
+                // --- this is post but no document is creatrd so return 200 
+                return res.sendStatus(200);
+            }
+            // --- user not found
+            return res.sendStatus(404);
+        });
+    });
+};
+
+
+function signUp(req, res) {
+
+    console.log("/users/signUp is accessed");
+
+    MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }
+
+        const dbo = db.db(dbName);
+        // --- expecting email , password...
+        const queryUser = req.body;
+
+        dbo.collection(collectionName).findOne({ 
+            userName:queryUser.userName, email:queryUser.email, password:queryUser.password, confirmPassword:queryUser.confirmPassword },
+             function (err, userFound) {
+            if (err) {
+                console.log(err);
+                return res.sendStatus(500);
+            }
+
+            if (userFound) {
+                // --- email found 
+                return res.sendStatus(400);
+            }
+
+            // --- no email match --> insert user
+            dbo.collection(collectionName).insertOne(queryUser, function (err, result) {
+                if (err) {
+                    console.log(err);
+                    return res.sendStatus(500);
+                }
+                res.sendStatus(201);
+            });
+
+        });
+    });
+
+}
+
+module.exports.signUp = signUp;
+module.exports.login = login;
--- server/server.js
+++ server/server.js
@@ -1,34 +1,43 @@
 const express = require("express");
 const bodyParser = require('body-parser');
-const router = express.Router();
+// const router = express.Router();
 const app = express();
+const routeHelper = require('./routeHelper')
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
 
-// Data base
-const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017/cinema";
-
-MongoClient.connect(url, function (err, db) {
-    if (err) throw err;
-    var dbo = db.db("cinema");
-    app.get('/', (req, res) => {
-        res.send('"hii');
-    });
-
-    app.post('/account/users', (req, res) => {
-        console.log(req.body);
-        dbo.collection("users").insertOne(req.body, (err, user)=>{
-            if (err) {
-                console.log(err);
-                res.send("there is some issue");
-            }
-            res.send(user);
-        })
-    });
+// New code
+app.post("/account/users/signUp", (req, res) => {
+    routeHelper.signUp(req, res);
 });
+app.post("/account/users/login", (req, res) => {
+    routeHelper.login(req, res);
+});
+
+// Data base
+// const MongoClient = require('mongodb').MongoClient;
+// const url = "mongodb://localhost:27017/cinema";
+
+// MongoClient.connect(url, function (err, db) {
+//     if (err) throw err;
+//     var dbo = db.db("cinema");
+//     app.get('/', (req, res) => {
+//         res.send('"hii');
+//     });
+
+//     app.post('/account/users', (req, res) => {
+//         console.log(req.body);
+//         dbo.collection("users").insertOne(req.body, (err, user)=>{
+//             if (err) {
+//                 console.log(err);
+//                 res.send("there is some issue");
+//             }
+//             res.send(user);
+//         })
+//     });
+// });
 
 // Port
 const port = process.env.PORT || 5000;
