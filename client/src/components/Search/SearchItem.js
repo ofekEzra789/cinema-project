520a1f67a12f8f94452c92526233cf732a8ee31a
iamofek <ofekofek789@gmail.com>
2020-01-28 09:35:42 +0200

    adding change icon to wishList

--- client/src/components/Search/SearchItem.js
+++ client/src/components/Search/SearchItem.js
@@ -2,20 +2,30 @@ import React, { Component } from 'react';
 import './SearchItem.css';
 import {
     Card, CardImg, CardText, CardBody,
-    Button, CardTitle
+    CardTitle
 } from 'reactstrap';
 
 export class SearchItem extends Component {
     constructor(props) {
         super(props);
         this.handleAddMovie = this.handleAddMovie.bind(this)
+        this.state = {
+            onWishList: false
+        }
     }
 
     handleAddMovie() {
         this.props.addMovie(this.props.id, this.props.title, this.props.src, this.props.releaseDate, this.props.rating);
+        this.setState({ onWishList: true })
     }
 
     render() {
+        let iconHeart;
+        if(this.state.onWishList) {
+            iconHeart = <i className="em em-heart ml-2" aria-label="HEAVY BLACK HEART"></i>
+        } else {
+            iconHeart = <i onClick={this.handleAddMovie} className="em em-black_heart ml-2"  aria-label="BLACK HEART"></i>
+        }
         return (
             <div className="SearchItem">
                 <Card className="SearchItem-Card">
@@ -25,8 +35,8 @@ export class SearchItem extends Component {
                             <CardText>Release Year: {this.props.releaseDate}</CardText>
                             <CardText>Rating: {this.props.rating}</CardText>
                             <div className="card-Button">
-                                <Button>Buy Ticket </Button>
-                                <i onClick={this.handleAddMovie} className="em em-black_heart ml-2"  aria-label="BLACK HEART"></i>
+                                {/* <Button>Buy Ticket </Button> */}
+                                {iconHeart}
                             </div>
                         </CardBody>
                     </Card>
