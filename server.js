63a3f016c748e49030f596d7ca368a7146e35f88
iamofek <ofekofek789@gmail.com>
2020-02-18 16:01:46 +0200

    deploy

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const onWishList = require("./onWishList");
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // SignUp
 app.post("/account/users/signUp", (req, res) => {
