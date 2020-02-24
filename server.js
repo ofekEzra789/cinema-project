6e6cf9c4ad0545356184b953f142cdd4b0cec279
iamofek <ofekofek789@gmail.com>
2020-02-24 11:52:25 +0200

    add to server

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use(function (req, res, next){
   if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req.url);
+    res.redirect('https://' + req.hostname + req.url);
   } else {
     next();
   }
