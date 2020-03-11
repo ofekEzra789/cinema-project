42e75aeec90a7878357f14665467fe84feec38c7
iamofek <ofekofek789@gmail.com>
2020-03-11 14:27:21 +0200

    fixing server problems

--- server.js
+++ server.js
@@ -55,5 +55,5 @@ if (process.env.NODE_ENV == "production" || true) {
 
 }
 // Port
-const port = process.env.MONGOLAB_URI|| 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
